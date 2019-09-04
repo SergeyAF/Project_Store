8cc329d4c8cf7861ecb76838e34d94fced2a9c00
mark savenkov <mark.savenkov1993@gmail.com>
2019-09-04 20:14:39 +0200

    last changes 3.09

--- markup/src/js/pages/home.js
+++ markup/src/js/pages/home.js
@@ -1,4 +1,32 @@
 // eslint-disable-next-line func-names
+
+
+$(function(){
+  rangeSlider();
+});
+
+function rangeSlider() {
+  console.log('привет!');
+
+  if ($('#price-range').length) {
+      $('#price-range').ionRangeSlider({
+          type: 'double',
+          grid: false,
+          min: 0,
+          max: 1000,
+          from: 0,
+          to: 600,
+          prefix: "$",
+          hide_min_max: true
+      });
+  }
+}
+
 $(function () {
   $('#tabs').tabs();
 });
+
+
+
+
+
