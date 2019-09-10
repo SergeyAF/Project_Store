6af2776e3442c77803f9845a947a5f6af0d5129b
Vitaliy <pritula.vitaly94@gmail.com>
2019-09-10 09:43:35 +0300

    my page

--- markup/src/js/pages/home.js
+++ markup/src/js/pages/home.js
@@ -1,3 +1,8 @@
-'use strict';
-
-let parJan = 3;
+$(function () {
+  $('#tab').tabs({
+    activate: function (event, ui) {
+      ui.oldTab.removeClass('active-tab');
+      ui.newTab.addClass('active-tab');
+    },
+  });
+});
